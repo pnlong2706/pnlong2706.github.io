b32d19c18fe23ec4c1224fd3dc51d9b7dd6b5beb
Pham Ngoc Long <pnlong27@gmail.com>
2022-08-27 20:29:46 +0700

    upload

---
+++ buytable.js
@@ -0,0 +1,11 @@
+
+  function showTable() {
+    document.getElementById("he").style.display="flex"; 
+    document.getElementById("he").style.justifyContent="center";
+}
+
+    function closeTable() {
+        document.getElementById("he").style.display="none"; 
+    }
+
+ 
