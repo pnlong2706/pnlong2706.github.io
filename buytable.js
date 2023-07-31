548e3fb9d9af5e6b4abe849f1a9aa7d8dc262b73
Pham Ngoc Long <77040324+pnlong2706@users.noreply.github.com>
2023-07-31 21:38:10 +0700

    Update buytable.js

--- buytable.js
+++ buytable.js
@@ -1,11 +1,33 @@
 
-  function showTable() {
+function showTable_1() {
     document.getElementById("he").style.display="flex"; 
     document.getElementById("he").style.justifyContent="center";
+    document.getElementById("he-head").innerHTML="The maze"; 
+    document.getElementById("content_box_he").innerHTML="This web was made mainly by HTML, CSS, and JS, like the usual maze, you have to move your character to the endpoint, be careful that your step is limited. This has just one maze, generate new maze functions are in development!";
+    document.getElementById("pay").href="https://lota2706.github.io";
+    document.getElementById("pay").innerHTML="Visit ✔";
 }
 
-    function closeTable() {
+function showTable_2() {
+  document.getElementById("he").style.display="flex"; 
+  document.getElementById("he").style.justifyContent="center";
+  document.getElementById("he-head").innerHTML="Lights out"; 
+  document.getElementById("content_box_he").innerHTML="In this game, when you click on a bulb on position (i,j), all the bulbs on (i-1,j); (i,j), (i+1,j), (i,j+1), (i,j-1) will change their state ( on to off and opposite ), you have to make all the lights on or off!"; 
+  document.getElementById("pay").href="https://longlong2706.github.io";
+  document.getElementById("pay").innerHTML="Visit ✔";
+}
+
+function showTable_3() {
+  document.getElementById("he").style.display="flex"; 
+  document.getElementById("he").style.justifyContent="center";
+  document.getElementById("he-head").innerHTML="Only chair"; 
+  document.getElementById("content_box_he").innerHTML="This forum is all about chairs, you can create an account and post your opinion, and others will discuss that, also, you can sell or buy chairs here. This web will be available in the future soon. ";
+  document.getElementById("pay").href="";
+  document.getElementById("pay").innerHTML="Not available yet!";
+}
+
+function closeTable() {
         document.getElementById("he").style.display="none"; 
-    }
+}
 
  
